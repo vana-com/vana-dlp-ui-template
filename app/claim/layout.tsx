b976c5167b2d09124fc584490a18b74ba43f0ef9
Volod <volod@vana.com>
2024-10-01 23:52:37 -0400

    feat: cleanup, remove unused components

--- app/claim/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { Box } from "@mantine/core";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <Box py={{ base: "16", sm: "72" }}>{children}</Box>;
-}
