aa414032815a570d9428f584ff0f4a9e0c3d1b21
Volod <volod@vana.com>
2024-10-03 16:36:06 -0400

    feat: simplify config

--- app/config.ts
+++ app/config.ts
@@ -5,9 +5,6 @@ const networks = {
     chainName: "Vana Moksha Testnet",
     explorerUrl: "https://moksha.vanascan.io",
     currency: "VANA",
-    contract: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_MOKSHA_TESTNET || "moksha",
-    dataRegistryContract: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_DATA_REGISTRY_MOKSHA_TESTNET || "data_registry",
-    teePoolContract: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_TEE_POOL_MOKSHA_TESTNET || "tee_pool",
   },
   satori: {
     chainId: "14801",
@@ -15,9 +12,6 @@ const networks = {
     chainName: "Vana Satori Testnet",
     explorerUrl: "https://satori.vanascan.io",
     currency: "VANA",
-    contract: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_SATORI_TESTNET || "satori",
-    dataRegistryContract: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_DATA_REGISTRY || "0xEA882bb75C54DE9A08bC46b46c396727B4BFe9a5",
-    teePoolContract: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_TEE_POOL || "0xF084Ca24B4E29Aa843898e0B12c465fAFD089965",
   },
   mainnet: {
     chainId: "1480",
@@ -25,13 +19,10 @@ const networks = {
     chainName: "Vana Mainnet",
     explorerUrl: "https://vanascan.io",
     currency: "VANA",
-    contract: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_MAINNET || "",
-    dataRegistryContract: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_DATA_REGISTRY_MAINNET || "",
-    teePoolContract: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_TEE_POOL_MAINNET || "",
   },
 }
 
-const network = (process.env.NEXT_PUBLIC_NETWORK || "satori") as keyof typeof networks;
+const network = (process.env.NEXT_PUBLIC_NETWORK || "moksha") as keyof typeof networks;
 
 if (!Object.keys(networks).includes(network)) {
   throw new Error(`Invalid network type: ${network}`);
@@ -43,24 +34,20 @@ if (!networkConfig) {
   networks[network] = networkConfig;
 }
 
-networkConfig.contract = process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS || networkConfig.contract;
-networkConfig.dataRegistryContract = process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_DATA_REGISTRY || networkConfig.dataRegistryContract;
-networkConfig.teePoolContract = process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_TEE_POOL || networkConfig.teePoolContract;
-networkConfig.rpcUrl = process.env.NEXT_PUBLIC_RPC_URL || networkConfig.rpcUrl;
-networkConfig.chainName = process.env.NEXT_PUBLIC_CHAIN_NAME || networkConfig.chainName;
-networkConfig.explorerUrl = process.env.NEXT_PUBLIC_EXPLORER_URL || networkConfig.explorerUrl;
-networkConfig.currency = process.env.NEXT_PUBLIC_CURRENCY || networkConfig.currency;
-
 const config = {
   dropboxCallbackUrl: process.env.NEXT_PUBLIC_DROPBOX_CALLBACK_URL || "",
   dropboxClientId: process.env.NEXT_PUBLIC_DROPBOX_CLIENT_ID || "",
   dropboxFolderName: "data-dao",
   googleDriveFolderName: "data-dao",
   googleClientId: process.env.NEXT_PUBLIC_GOOGLE_DRIVE_CLIENT_ID || "",
-
   publicKeyBase64: process.env.NEXT_PUBLIC_FILE_ENCRYPTION_PUBLIC_KEY_BASE64 || "",
-
   network,
+  networkConfig,
+  smartContracts: {
+  dlp: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_DLP || "0x88006Bc06d3B703a3F50ACe4DEFC587549085940",
+    dataRegistry: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_DATA_REGISTRY || "0xEA882bb75C54DE9A08bC46b46c396727B4BFe9a5",
+    teePool: process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS_TEE_POOL || "0xF084Ca24B4E29Aa843898e0B12c465fAFD089965",
+  }
 };
 
 export { config, networks };
